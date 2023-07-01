a348ffba3b55459d48565642a3ba3fd445a81dbb
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-01 22:24:54 +0530

    Creating a sample get post end point or path operation

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message" : "Hello World"}
+    return {
+            "message" : "Welcome to my API made with FastAPI!!!",
+            "name" : "neo"
+        }
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "A demo post"}
