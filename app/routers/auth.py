6af08c28dbf58cf8c8bde334acc4374ada8a4c2a
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-10 19:38:55 +0530

    Setting up token creation on login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from ..database import get_db
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 from sqlalchemy.orm import Session
 
 router = APIRouter(
@@ -17,4 +17,8 @@ async def login(user_cred: schemas.UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
     
     # create a token
-    return {"token" : "example"}
+    access_token = oauth2.create_access_token(data={"user_id" : user.id})
+
+    return {"access_token" : access_token, "token_type" : "bearer"}
+
+
