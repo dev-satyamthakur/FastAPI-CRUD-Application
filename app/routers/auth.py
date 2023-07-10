f26a4c92c36abed6a6b7071ad66836ea37d8c7da
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-10 19:46:40 +0530

    Modifying login endpoint user details post to OAuth2PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
 from .. import schemas, models, utils, oauth2
 from sqlalchemy.orm import Session
@@ -8,8 +9,10 @@ router = APIRouter(
 )
 
 @router.post("/login")
-async def login(user_cred: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_cred.email).first()
+async def login(user_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.email == user_cred.username).first()
+    
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
 
