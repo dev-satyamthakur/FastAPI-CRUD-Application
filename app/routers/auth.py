469b096aeba0f4aa636c9bc2f27f0c2030671fd1
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-11 19:27:00 +0530

    Updating HTTP exception response code in auth.py file

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -14,10 +14,10 @@ async def login(user_cred: OAuth2PasswordRequestForm = Depends(), db: Session = 
     user = db.query(models.User).filter(models.User.email == user_cred.username).first()
     
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     if not utils.verify(user_cred.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
     # create a token
     access_token = oauth2.create_access_token(data={"user_id" : user.id})
