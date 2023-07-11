3ab8efd067b2ee9095f68beb3aa48c6a16ee1c0a
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-11 15:13:19 +0530

    Creating get_current_user funciton in oauth2 file

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "mykey12120909" 
 ALGORITHM = "HS256"
@@ -25,4 +29,10 @@ def verify_access_token(token: str, credentials_exception):
         token_data = schemas.TokenData(id=id)
     except JWTError:
         raise credentials_exception
+    
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate" : "Bearer"})
+    
+    return verify_access_token(token=token, credentials_exception=credentials_exception)
             
