e9cdab346cbed0bfa3bd69cccda945c615760856
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-15 20:10:13 +0530

    Removed tracked files which shouldn't be tracked

--- app/config.py
+++ app/config.py
@@ -10,5 +10,8 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
     
 settings = Settings()
